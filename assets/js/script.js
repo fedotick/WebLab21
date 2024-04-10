334803ebba5e595bf1ddbb7a4a7d81bf75257955
fedotick <feodortcaciuc2004@gmail.com>
2024-04-10 21:00:09 +0300

    Task 1 updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ const btnSquareTheNumber = document.getElementById('btnSquareTheNumber');
 btnSquareTheNumber.addEventListener('click', squareTheNumber);
 
 function squareTheNumber() {
-    const inputNumber = document.getElementById('number');
+    const inputNumber = document.getElementById('inputNumber');
     const number = inputNumber.value;
 
     alert(`The square of ${number} is ${number * number}`);
