f1d1a9511217851137877829e5213e3203db0caf
fedotick <feodortcaciuc2004@gmail.com>
2024-04-10 21:04:06 +0300

    Task 2 complited

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,15 @@ function squareTheNumber() {
 
     alert(`The square of ${number} is ${number * number}`);
 }
+
+// Task 2
+const btnSwap = document.getElementById('btnSwap');
+
+btnSwap.addEventListener('click', swapValues);
+
+function swapValues() {
+    const inputFirst = document.getElementById('inputFirst');
+    const inputSecond = document.getElementById('inputSecond');
+
+    [inputFirst.value, inputSecond.value] = [inputSecond.value, inputFirst.value];
+}
